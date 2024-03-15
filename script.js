936710b582c6982a7ac80d551160b187f8d3832d
Kabtzon <112047257+Kabtzon@users.noreply.github.com>
2024-03-14 23:30:51 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var toggleMenu = document.getElementById('toggleMenu');
-    var sideMenu = document.getElementById('sideMenu');
-
-    toggleMenu.addEventListener('click', function() {
-        sideMenu.classList.toggle('active');
-        toggleMenu.classList.toggle('expanded');
-    });
-});
